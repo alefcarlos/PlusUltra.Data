c65c1720fea20c0589925b2122c5d4c2593be2b4
Alef Carlos <alef.carlos@gmail.com>
2019-10-13 13:09:37 -0300

    feat: suporte a certificados

---
+++ src/PlusUltra.Data.SqlKata.PostgresSQL/PostgresSettings.cs
@@ -0,0 +1,7 @@
+namespace PlusUltra.Data.SqlKata.PostgresSQL
+{
+    public class PostgresSettings
+    {
+        public string ClientCertPath { get; set; }
+    }
+}
--- src/PlusUltra.Data.SqlKata.PostgresSQL/RegisterPostgresSQL.cs
+++ src/PlusUltra.Data.SqlKata.PostgresSQL/RegisterPostgresSQL.cs
@@ -1,6 +1,8 @@
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Npgsql;
 using SqlKata.Compilers;
 using SqlKata.Execution;
@@ -13,13 +15,28 @@ namespace PlusUltra.Data.SqlKata.PostgresSQL
         {
             var logger = logFactory.CreateLogger("PostgressSQL");
 
+            services.Configure<PostgresSettings>(configuration.GetSection(nameof(PostgresSettings)));
+
             services.AddScoped((provider) =>
             {
+                var settings = provider.GetRequiredService<IOptions<PostgresSettings>>().Value;
+
                 var connection = new NpgsqlConnection(configuration.GetConnectionString("PostgreSQL"));
+                
+                void ProvideClientCertificates(X509CertificateCollection clientCerts)
+                {
+                    var clientCertPath = settings.ClientCertPath;
+                    var cert = new X509Certificate2(clientCertPath);
+                    clientCerts.Add(cert);
+                }
+
+                if (!string.IsNullOrEmpty(settings.ClientCertPath))
+                    connection.ProvideClientCertificatesCallback += ProvideClientCertificates;
 
                 var compiler = new PostgresCompiler();
 
-                return new QueryFactory(connection, compiler){
+                return new QueryFactory(connection, compiler)
+                {
                     Logger = compiled => logger.LogInformation(compiled.ToString())
                 };
             });
